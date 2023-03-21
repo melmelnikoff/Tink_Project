ddc745eefa3fdaec3916475cf802777dcdab7342
melmelnikoff <mel1719@mail.ru>
2023-03-21 09:24:03 +0500

    Задание 2.Мелкие исправления

--- bot/src/main/java/ru/tinkoff/edu/java/bot/BotApplication.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/BotApplication.java
@@ -9,7 +9,7 @@ import ru.tinkoff.edu.java.scrapper.ScrapperApplication;
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class BotApplication {
 public static void main(String[] args) {
-        var ctx = SpringApplication.run(ScrapperApplication.class, args);
+        var ctx = SpringApplication.run(BotApplication.class, args);
         ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
         System.out.println(config);
         }
